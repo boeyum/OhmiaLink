d149e34a435296f900deb7917bf475e4168c9312
Jan-Ivar Bøyum <78592574+boeyum@users.noreply.github.com>
2021-12-13 13:27:34 +0100

    Add files via upload

---
+++ app/build/generated/source/buildConfig/debug/jib/no/app/ohmialink/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package jib.no.app.ohmialink;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "jib.no.app.ohmialink";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
